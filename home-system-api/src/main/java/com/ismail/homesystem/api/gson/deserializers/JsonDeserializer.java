96799d7a73a7c37e0b7ad67b21d6f127eadf18ed
ismaiiil <ismaiiil@live.com>
2023-06-17 02:04:30 +0400

    testing config file

--- home-system-api/src/main/java/com/ismail/homesystem/api/gson/deserializers/JsonDeserializer.java
+++ home-system-api/src/main/java/com/ismail/homesystem/api/gson/deserializers/JsonDeserializer.java
@@ -3,15 +3,18 @@ package com.ismail.homesystem.api.gson.deserializers;
 import com.google.gson.Gson;
 import com.ismail.homesystem.api.gson.models.DatabaseConfig;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.FileSystems;
 
 public class JsonDeserializer {
 
     private DatabaseConfig databaseConfig;
 
-    public JsonDeserializer(String path) {
-        try (FileReader reader = new FileReader(path)) {
+    public JsonDeserializer(String jsonFilename) {
+        String workingDirectory = new File("").getAbsolutePath();
+        try (FileReader reader = new FileReader(workingDirectory + FileSystems.getDefault().getSeparator() + jsonFilename)) {
             Gson gson = new Gson();
             databaseConfig = gson.fromJson(reader, DatabaseConfig.class);
         }catch (IOException e){
